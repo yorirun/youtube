e7afe720a1aac276c623e49e76ef36e902f528fa
yorirun <117011631+yorirun@users.noreply.github.com>
2023-03-26 16:28:03 +0900

    prototype-01.c
    prototype-01.c

---
+++ C로 요리보리/prototype-01.c
@@ -0,0 +1,8 @@
+#include <stdio.h> // 출력구문, 기본적인 역할 --> printf의 재료
+#include <iostream> //cmd 불러오는 친구 --> 검은 창 띄어줌 (안써도 출력되긴 함--자체 디버거가 vs에 있음)
+
+void main() { //main함수 실행 영역 (이 구역 안에서 코드가 실행됨) --> 집 터와 같음
+	printf_s("Hello World!"); //출력 , 끝내기 = (;) + 원하는 말을 쓰면 원하는 말이 작성됨 ex) printf_s("Hi"); --> Hi
+}
+
+ // <--주석 : 실행할 때 무시하게해줌
